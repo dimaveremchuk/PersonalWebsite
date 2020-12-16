92d8ffefc3d61980206760e8becf4391a7b636a3
Dima Veremchuk <72143721+dmitryveremchuk@users.noreply.github.com>
2020-12-16 06:38:56 +0300

    Reset CSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dima Veremchuk`,
+    description: `My personal website.`,
+    author: `@dimaveremchuk`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
